f06b669d5d2a55ac6a8d5ab45d2649e305f8e552
Cara O'Shea <oshea.cara@gmail.com>
2018-02-18 23:24:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function happyHolidays()
   
 function happyHolidaysTo(name) 
 {
-  return (`Happy holidays ${name}!`)
+  return (`Happy holidays, ${name}!`)
 }
 
 
-function happyHolidaysTo(holiday, name)
+function happyHolidayTo(holiday, name)
 {
   return (`Happy ${holiday}, ${name}!`)
 }
